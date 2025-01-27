f81b8a07f11c4f0f23c09637e6affb714e809c98
samuel Dejesus <samuel1de2jesus3@gmail.com>
2025-01-27 15:27:50 -0500

    made the icons clickable and so they would reroute to their respective pages

--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import dateHelper from '../components/helper functions/DateTime'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGear,faBell } from '@fortawesome/free-solid-svg-icons';
+
+
 
 
 const today = dateHelper(new Date())
 
 export default function Homepage() {
+    const n = useNavigate();
     return (
         <div className='homepageContainer col-11 mt-5 bg-info'>
             {/* header section*/}
@@ -22,10 +28,18 @@ export default function Homepage() {
             </p>
             
             <p>
-                Notification
+            <FontAwesomeIcon
+            onClick={() => n('/Notifications')}
+            icon={faBell} 
+            style={{ cursor: 'pointer' }}
+            />
             </p>
             <p>
-                Settings
+            <FontAwesomeIcon
+            icon={faGear} 
+            onClick={() => n('/Settings')}
+            style={{ cursor: 'pointer'}}
+            />
             </p>
             </div>
             
